ee4099ec90c010ecf63f94711226f458bae02bea
t-kida-tribeat <t-kida@tribeat.com>
2025-07-04 16:38:14 +0900

    再アップ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    preset: 'cloudflare_pages' // 明示的に指定
+  }
 })
